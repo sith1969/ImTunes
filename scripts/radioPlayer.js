a323c35981447c389c189ed1af409082bb325c40
sith1969 <sith@tut.by>
2020-07-29 17:17:16 +0300

    Урок 3 RadioPlayer
    Lesson 3 RadioPlayer

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,63 @@
 export const radioPlayerInit= () => {
-    console.log("Radio");
+   console.log("Radio");
+
+   const radio = document.querySelector('.radio');
+   const radioCoverImg= document.querySelector('.radio-cover__img');
+   const radioNavigation= document.querySelector('.radio-navigation');
+   const radioHeaderBig= document.querySelector('.radio-header__big');
+   const radioItem= document.querySelectorAll('.radio-item');
+   const radioStop= document.querySelector('.radio-stop');
+
+   const audio = new Audio();
+   audio.type ='audio/aac';
+
+   radioStop.disabled = true;
+
+   const changeIconPlay = () => {
+      if (audio.paused){
+         radio.classList.remove('play')
+         radioStop.classList.add('fa-play');
+         radioStop.classList.remove('fa-stop');
+         
+      }else {
+         radio.classList.add('play')
+         radioStop.classList.remove('fa-play');
+         radioStop.classList.add('fa-stop');
+         
+      } 
+   };
+
+   const selectItem = elem => {
+      radioItem.forEach(item =>item.classList.remove('select'))
+      elem.classList.add('select');
+   };
+
+   radioNavigation.addEventListener('change',(event) =>{
+      const target= event.target;
+      //console.log(target.dataset.radioStantion);
+
+      const parrent = target.closest('.radio-item');
+      selectItem(parrent);
+      const title =parrent.querySelector('.radio-name').textContent;
+      radioHeaderBig.textContent = title;
+
+      const urlImg =parrent.querySelector('.radio-img').src;
+      radioCoverImg.src = urlImg;
+
+      radioStop.disabled = false;
+
+      audio.src =target.dataset.radioStantion;
+      audio.play();
+      changeIconPlay();
+   });
+
+   radioStop.addEventListener('click',() => {
+      if (audio.paused){
+         audio.play();
+      }else {
+         audio.pause();
+      }
+      changeIconPlay();
+   });
+
 };
